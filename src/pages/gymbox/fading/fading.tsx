4df506c3aede50b7d1bdfd1a8d8a265910495933
siyoon <siyoon@friendli.ai>
2023-08-05 01:20:31 +0900

    fading texts

--- src/pages/gymbox/fading/fading.tsx
+++ src/pages/gymbox/fading/fading.tsx
@@ -1,133 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
-import classes from "./fading.module.scss";
+import Fading, { FadingProp } from "../../../views/fading-text/fading";
 
 const texts = ["나는", "날지", "않아", "그건", "유산소거든"];
 
-function Fading() {
-  const text1Ref = useRef<HTMLSpanElement | null>(null);
-  const text2Ref = useRef<HTMLSpanElement | null>(null);
-
-  useEffect(() => {
-    if (!text1Ref.current || !text2Ref.current) return;
-
-    const morphTime = 1;
-    const cooldownTime = 0.25;
-
-    let textIndex = texts.length - 1;
-    let morph = 0;
-    let cooldown = cooldownTime;
-    let time = new Date();
-
-    text1Ref.current.textContent = texts[textIndex % texts.length];
-    text2Ref.current.textContent = texts[(textIndex + 1) % texts.length];
-
-    const setMorph = (fraction: number) => {
-      if (!text1Ref.current || !text2Ref.current) return;
-
-      text2Ref.current.style.filter = `blur(${Math.min(
-        8 / fraction - 8,
-        100
-      )}px)`;
-      text2Ref.current.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-      const fraction2 = 1 - fraction;
-      text1Ref.current.style.filter = `blur(${Math.min(
-        8 / fraction2 - 8,
-        100
-      )}px)`;
-      text1Ref.current.style.opacity = `${Math.pow(fraction2, 0.4) * 100}%`;
-
-      text1Ref.current.textContent = texts[textIndex % texts.length];
-      text2Ref.current.textContent = texts[(textIndex + 1) % texts.length];
-    };
-
-    function doMorph() {
-      morph -= cooldown;
-      cooldown = 0;
-
-      let fraction = morph / morphTime;
-
-      if (fraction > 1) {
-        cooldown = cooldownTime;
-        fraction = 1;
-      }
-
-      setMorph(fraction);
-    }
-
-    function doCooldown() {
-      if (!text1Ref.current || !text2Ref.current) return;
-      morph = 0;
-
-      text2Ref.current.style.filter = "";
-      text2Ref.current.style.opacity = "100%";
-
-      text1Ref.current.style.filter = "";
-      text1Ref.current.style.opacity = "0%";
-    }
-
-    function animate() {
-      requestAnimationFrame(animate);
-
-      const newTime = new Date();
-      const shouldIncrementIndex = cooldown > 0;
-      const dt = (newTime.getTime() - time.getTime()) / 1000;
-      time = newTime;
-
-      cooldown -= dt;
-
-      if (cooldown <= 0) {
-        if (shouldIncrementIndex) {
-          textIndex++;
-        }
-
-        doMorph();
-      } else {
-        doCooldown();
-      }
-    }
-
-    animate();
-
-    return () => animate();
-  }, []);
-
-  return (
-    <>
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "black",
-          width: "100vw",
-          height: "100%",
-        }}
-      />
-      <div
-        className={classes.container}
-        style={{ backgroundColor: "transparent" }}
-      >
-        <span ref={text1Ref} className={classes.text} />
-        <span ref={text2Ref} className={classes.text} />
-      </div>
-      <svg className={classes.filters}>
-        <defs>
-          <filter id="threshold">
-            <feColorMatrix
-              in="SourceGraphic"
-              type="matrix"
-              values="1 0 0 0 255
-                0 1 0 0 0
-                0 0 1 0 0
-                0 0 0 255 -155"
-            />
-          </filter>
-        </defs>
-      </svg>
-    </>
-  );
+function FadingPage() {
+  const fadingProps: FadingProp = {
+    width: "100vw",
+    height: "100%",
+    textColor: {
+      R: 0,
+      G: 0.1,
+      B: 0.1,
+    },
+    backgroundColor: "whitesmoke",
+    texts,
+  };
+  return <Fading {...fadingProps} />;
 }
 
-Fading.displayName = "Fading";
+FadingPage.displayName = "FadingPage";
 
-export default Fading;
+export default FadingPage;
---
+++ src/views/fading-text/fading.tsx
@@ -0,0 +1,176 @@
+import React, { CSSProperties, useEffect, useRef } from "react";
+
+export interface FadingProp {
+  texts: string[];
+  width: CSSProperties["width"];
+  height: CSSProperties["height"];
+  backgroundColor?: CSSProperties["backgroundColor"];
+  textColor?: {
+    R?: number;
+    G?: number;
+    B?: number;
+  };
+  morphTime?: number;
+  cooldownTime?: number;
+  fontSize?: CSSProperties["fontSize"];
+  fontFamily?: CSSProperties["fontFamily"];
+}
+
+function Fading(props: FadingProp) {
+  const text1Ref = useRef<HTMLSpanElement | null>(null);
+  const text2Ref = useRef<HTMLSpanElement | null>(null);
+  const radId = useRef<number>();
+
+  const fontStyle: CSSProperties = {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    textAlign: "center",
+    width: "100%",
+    fontSize: props.fontSize || "6rem",
+    fontFamily:
+      `${props.fontFamily ? `${props.fontFamily},` : ""}` + "Hevletica",
+  };
+
+  const {
+    backgroundColor = "white",
+    textColor = { R: 0, G: 0, B: 0 },
+    texts,
+  } = props;
+
+  useEffect(() => {
+    if (!text1Ref.current || !text2Ref.current) return;
+
+    const morphTime = props.morphTime || 1;
+    const cooldownTime = props.cooldownTime || 0.25;
+
+    let textIndex = texts.length - 1;
+    let morph = 0;
+    let cooldown = cooldownTime;
+    let time = new Date();
+
+    text1Ref.current.textContent = texts[textIndex % texts.length];
+    text2Ref.current.textContent = texts[(textIndex + 1) % texts.length];
+
+    const setMorph = (fraction: number) => {
+      if (!text1Ref.current || !text2Ref.current) return;
+
+      text2Ref.current.style.filter = `blur(${Math.min(
+        8 / fraction - 8,
+        100
+      )}px)`;
+      text2Ref.current.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+      const fraction2 = 1 - fraction;
+      text1Ref.current.style.filter = `blur(${Math.min(
+        8 / fraction2 - 8,
+        100
+      )}px)`;
+      text1Ref.current.style.opacity = `${Math.pow(fraction2, 0.4) * 100}%`;
+
+      text1Ref.current.textContent = texts[textIndex % texts.length];
+      text2Ref.current.textContent = texts[(textIndex + 1) % texts.length];
+    };
+
+    function doMorph() {
+      morph -= cooldown;
+      cooldown = 0;
+
+      let fraction = morph / morphTime;
+
+      if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+      }
+
+      setMorph(fraction);
+    }
+
+    function doCooldown() {
+      if (!text1Ref.current || !text2Ref.current) return;
+      morph = 0;
+
+      text2Ref.current.style.filter = "";
+      text2Ref.current.style.opacity = "100%";
+
+      text1Ref.current.style.filter = "";
+      text1Ref.current.style.opacity = "0%";
+    }
+
+    function animate() {
+      radId.current = requestAnimationFrame(animate);
+
+      const newTime = new Date();
+      const shouldIncrementIndex = cooldown > 0;
+      const dt = (newTime.getTime() - time.getTime()) / 1000;
+      time = newTime;
+
+      cooldown -= dt;
+
+      if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+          textIndex++;
+        }
+
+        doMorph();
+      } else {
+        doCooldown();
+      }
+    }
+
+    animate();
+
+    return () => cancelAnimationFrame(radId.current || 0);
+  }, [props]);
+
+  return (
+    <div
+      style={{
+        width: props.width,
+        height: props.height,
+        outline: "none",
+        touchAction: "none",
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundColor,
+        }}
+      />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          backgroundColor: "transparent",
+          filter: "url(#threshold) blur(0.0375rem)",
+        }}
+      >
+        <span ref={text1Ref} style={fontStyle} />
+        <span ref={text2Ref} style={fontStyle} />
+      </div>
+      <svg style={{ width: "0", height: "0", display: "none" }}>
+        <defs>
+          <filter id="threshold">
+            <feColorMatrix
+              in="SourceGraphic"
+              type="matrix"
+              values={`1 0 0 0 ${textColor.R}
+                0 1 0 0 ${textColor.G}
+                0 0 1 0 ${textColor.B}
+                0 0 0 255 -100`}
+            />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+Fading.displayName = "Fading";
+
+export default Fading;
